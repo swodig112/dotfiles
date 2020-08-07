aeba14066af384dd1d579ddb04b01777aff65369
swodig 112 <swodig112@mailfence.com>
2020-08-07 12:11:12 +0430

    Fix a typo

--- config-wizard.sh
+++ config-wizard.sh
@@ -7,7 +7,7 @@ yes | sudo pacman -Syu
 echo "$scriptname: Installing preferred packages..."
 yes | sudo pacman -S engrampa vim awesome nemo firefox xreader kitty zsh\
   telegram-desktop element-desktop atom lxdm xscreensaver vlc
-echo "$scriptname: Installing zsh..."
+echo "$scriptname: Installing ohmyzsh..."
 sh -c\
   "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "$scriptname: Installing atom editor packages..."
